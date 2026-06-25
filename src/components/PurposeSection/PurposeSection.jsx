e93dbea4436565fd9317927d96ed3728497027cc
Jonhycristian <wketapi@gmail.com>
2026-06-25 12:10:05 -0300

    style: increase max-widths for desktop, enhance hero button, make whatsapp button round, fix purpose image

--- src/components/PurposeSection/PurposeSection.jsx
+++ src/components/PurposeSection/PurposeSection.jsx
@@ -43,8 +43,8 @@ export function PurposeSection() {
         
         <div className={styles.imageWrapper}>
           <img 
-            src="https://images.unsplash.com/photo-1576092762791-dd9e2220afa1?q=80&w=2070&auto=format&fit=crop" 
-            alt="Atendimento Veterinário" 
+            src="https://images.unsplash.com/photo-1583337130417-3346a1be7dee?q=80&w=2000&auto=format&fit=crop" 
+            alt="Atendimento Veterinário Premium" 
             className={styles.image}
           />
         </div>
